9880bdbb3f30be66806929e9b2f16038971c3c5c
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2025-07-17 02:54:43 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="GeoHash Toolbox",
+    page_icon="🌍",
+    initial_sidebar_state="expanded"  # ✅ biar sidebar langsung terbuka
+)
+
+st.title("🌍 GeoHash Toolbox")
+st.markdown("""
+Selamat datang di **GeoHash Toolbox**!
+
+Gunakan menu di sidebar kiri:
+- 🔄 Area to GeoHash6
+- ➡️ CSV to GeoJSON
+- 🗺️ GeoJSON to CSv
+""")
