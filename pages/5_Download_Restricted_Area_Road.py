66db2e2c5cd44457c175fc14ef37b4370c663d6b
Nuril Hidayati <nurilassajdah@gmail.com>
2025-07-23 13:31:26 +0700

    update

--- pages/5_Download_Restricted_Area_Road.py
+++ pages/5_Download_Restricted_Area_Road.py
@@ -107,13 +107,4 @@ if st.session_state.show_road_download and st.session_state.buffer_road:
     st.download_button("⬇️ Download Roads", st.session_state.buffer_road,
                        f"{road_filename}.geojson", "application/geo+json")
 
-# Footer
-st.markdown(
-    """
-    <hr style="margin-top: 2rem; margin-bottom: 1rem;">
-    <div style='text-align: center; color: grey; font-size: 0.9rem;'>
-        © 2025 ID Karta IoT Team
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+
